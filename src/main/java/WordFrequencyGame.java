25a05be4f1b014bdfec7e270759ce1ab98846958
Theo Lo <theo.lo@oocl.com>
2020-11-30 16:13:43 +0800

    [WordFrequency][Theo]refactor: inline variables

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -15,8 +15,7 @@ public class WordFrequencyGame {
 
                 List<WordFrequency> wordFrequencyList = new ArrayList<>();
                 for (String word : words) {
-                    WordFrequency wordFrequency = new WordFrequency(word, 1);
-                    wordFrequencyList.add(wordFrequency);
+                    wordFrequencyList.add(new WordFrequency(word, 1));
                 }
 
                 //get the map for the next step of sizing the same word
@@ -24,8 +23,7 @@ public class WordFrequencyGame {
 
                 List<WordFrequency> wordCountList = new ArrayList<>();
                 for (Map.Entry<String, List<WordFrequency>> entry : wordCountMap.entrySet()) {
-                    WordFrequency wordFrequency = new WordFrequency(entry.getKey(), entry.getValue().size());
-                    wordCountList.add(wordFrequency);
+                    wordCountList.add(new WordFrequency(entry.getKey(), entry.getValue().size()));
                 }
 
                 wordCountList.sort((word1, word2) -> word2.getCount() - word1.getCount());
